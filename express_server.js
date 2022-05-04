9da5e5db12b1ec4164f0b325b766d2222b09c5f8
Carolyn Wang <Carolyn.Wang@ZEN-CWANG-M-L.local>
2022-05-04 17:39:34 -0400

    fixing formatting, add create new url, ejs

--- express_server.js
+++ express_server.js
@@ -18,13 +18,30 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("pages/urls_show", templateVars);
 });
+
+
+function generateRandomString() {
+
+  let randomString = Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 6);
+  return randomString
+
+}
